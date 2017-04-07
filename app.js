0f1e6d3097956a330aaf995538f8bdd072d16541
carynova <carychan317@gmail.com>
2017-04-07 14:43:27 +0800

    3.2 routes

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+var indexRouter = require('./routers/index')
+var usersRouter = require('./routers/users')
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+app.listen(3000)
