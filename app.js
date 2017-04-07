a06468b4a32fc645c9835ae31930055a34bf1483
carynova <carychan317@gmail.com>
2017-04-07 15:08:31 +0800

    use ejs tpl engine

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
+// template engine configs
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// routers
 var indexRouter = require('./routers/index')
 var usersRouter = require('./routers/users')
 
--- routers/users.js
+++ routers/users.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/:name', (req, res) => res.send('hello, ' + req.params.name))
+router.get('/:name', (req, res) => res.render('users', {
+  name: req.params.name
+}))
 
 module.exports = router
