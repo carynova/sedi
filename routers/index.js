73fc793c903098cd0db530d91fe522ef98e0b090
carynova <carychan317@gmail.com>
2017-04-08 16:11:26 +0800

    page design

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,9 @@
-var express = require('express')
-var router = express.Router()
-
-router.get('/', (req, res) => res.send('hello express'))
-
-module.exports = router
+module.exports = app => {
+  app.get('/', (req, res) => {
+    res.redirect('/posts')
+  })
+  app.use('/signup', require('./signup'))
+  app.use('/signin', require('./signin'))
+  app.use('/signout', require('./signout'))
+  app.use('/posts', require('./posts'))
+}
