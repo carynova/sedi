8887031ceee3003d8b327d3f8c38069da044b632
carynova <carychan317@gmail.com>
2017-04-12 13:36:52 +0800

    404 n error

--- routers/index.js
+++ routers/index.js
@@ -6,4 +6,11 @@ module.exports = app => {
   app.use('/signin', require('./signin'))
   app.use('/signout', require('./signout'))
   app.use('/posts', require('./posts'))
+
+  // 404 page
+  app.use((req, res) => {
+    if (!res.headersSent) {
+      res.status(404).render('404')
+    }
+  })
 }
